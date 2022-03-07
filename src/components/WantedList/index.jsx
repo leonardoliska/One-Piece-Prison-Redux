38abdbefadbc7bde78c421d2fe798650b1eb310f
leonardoliska <leo.liska@hotmail.com>
2022-03-07 15:40:45 -0300

    refactor: change home page cards to wantedlist component

--- src/components/WantedList/index.jsx
+++ src/components/WantedList/index.jsx
@@ -1,9 +1,14 @@
 import Container from "./styles"
 
-const WantedList = ({ children }) => (
+import Card from "../../components/Card"
+
+import image1 from "../../assets/img/Brook's_Concert_Wanted_Poster.png"
+import image2 from "../../assets/img/Jinbe's_Current_Wanted_Poster.png"
+
+const WantedList = () => (
     <Container>
-        <div></div>
-        <main>{children}</main>
+        <Card img={image1} />
+        <Card img={image2} />
     </Container>
 )
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,7 @@
 import WantedList from "../../components/WantedList"
-import Card from "../../components/Card"
-
-import image1 from "../../assets/img/Brook's_Concert_Wanted_Poster.png"
-import image2 from "../../assets/img/Jinbe's_Current_Wanted_Poster.png"
 
 const Home = () => {
-    return (
-        <WantedList>
-            <Card img={image1} />
-            <Card img={image2} />
-        </WantedList>
-    )
+    return <WantedList></WantedList>
 }
 
 export default Home
